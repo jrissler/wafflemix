c56dc39ca99d6859e0529bec164219949fbbd4d5
James Rissler <risslerjames@gmail.com>
2012-09-01 11:12:56 -0400

    Add the rack for dragonfly.

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -54,6 +54,8 @@ module Dummy
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.middleware.insert 0, 'Dragonfly::Middleware', :images
   end
 end
 
