a3284ec401ecc834e606785ff9385b713cef02f0
James Rissler <risslerjames@gmail.com>
2012-09-15 16:04:14 -0400

    Allow translation attributes.

--- app/models/wafflemix/page.rb
+++ app/models/wafflemix/page.rb
@@ -1,6 +1,6 @@
 module Wafflemix
   class Page < ActiveRecord::Base
-    attr_accessible :deletable, :draft, :link_url, :parent_id, :position, :show_in_menu, :skip_to_first, :title, :body
+    attr_accessible :deletable, :draft, :link_url, :parent_id, :position, :show_in_menu, :skip_to_first, :title, :body, :translations_attributes
 
     default_scope  :order => 'position ASC'
     scope :active, where(:draft => false, :show_in_menu => true)
