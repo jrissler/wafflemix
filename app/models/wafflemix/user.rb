3c0144781872922c3c49b02e0179802564a95d12
James Rissler <risslerjames@gmail.com>
2012-09-21 10:15:32 -0400

    Password shouldn't be accessor - may be bug with custom auth keys.

--- app/models/wafflemix/user.rb
+++ app/models/wafflemix/user.rb
@@ -1,8 +1,7 @@
 module Wafflemix
   class User < ActiveRecord::Base
     attr_accessor :login
-    attr_accessor :password
-    
+
     attr_accessible :email, :username, :password, :password_confirmation, :remember_me, :login
 
     devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
