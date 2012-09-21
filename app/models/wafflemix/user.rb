bf2ddcc213d3ca2a711ec13104a3105d27471485
James Rissler <risslerjames@gmail.com>
2012-09-21 10:02:45 -0400

    Another devise issue with incorrect 401 on session create.

--- app/models/wafflemix/user.rb
+++ app/models/wafflemix/user.rb
@@ -1,6 +1,8 @@
 module Wafflemix
   class User < ActiveRecord::Base
     attr_accessor :login
+    attr_accessor :password
+    
     attr_accessible :email, :username, :password, :password_confirmation, :remember_me, :login
 
     devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
