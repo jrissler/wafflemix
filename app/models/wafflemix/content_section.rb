048c991950d4fac04a5c3546fdf82cbc2dd075a5
James Rissler <risslerjames@gmail.com>
2012-09-23 17:18:30 -0400

    Simple find for content section.
    Needs improved.

--- app/models/wafflemix/content_section.rb
+++ app/models/wafflemix/content_section.rb
@@ -3,5 +3,15 @@ module Wafflemix
     attr_accessible :content, :contentable_id, :contentable_type, :name
 
     belongs_to :contentable, :polymorphic => true
+
+    def self.title
+    	self.name.downcase.parameterize
+    end
+
+		def self.content_for(name)
+			part = where(["name LIKE ?", "%#{name}%"]).first
+			part.content
+		end
+
   end
 end
