7e676638e30859dd19237d70903ea382c6044b04
James Rissler <risslerjames@gmail.com>
2012-09-25 10:30:55 -0400

    Remove login for wm users model.

--- app/models/wafflemix/user.rb
+++ app/models/wafflemix/user.rb
@@ -1,10 +1,9 @@
 module Wafflemix
   class User < ActiveRecord::Base
-    attr_accessor :login
 
-    attr_accessible :email, :username, :password, :password_confirmation, :remember_me, :login
+    attr_accessible :email, :username, :password, :password_confirmation, :remember_me
 
-    devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
+    devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 
     has_many :wafflemix_roles, :class_name => 'Wafflemix::Role'
     has_many :roles, :through => :wafflemix_roles
