6671d3a9d85944566e4ba97b47e50563c397af0a
James Rissler <risslerjames@gmail.com>
2012-09-25 09:57:12 -0400

    Remove our modules devise session controller.

--- app/controllers/wafflemix/sessions_controller.rb
+++
@@ -1,7 +0,0 @@
-require_dependency "wafflemix/application_controller"
-
-module Wafflemix
-  class SessionsController < ::Devise::SessionsController
-    # the rest is inherited, so it should work
-  end
-end
