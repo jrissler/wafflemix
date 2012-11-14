ad2808ef5eeb3235bae966ff9f3e46d98ac130ac
James Rissler <risslerjames@gmail.com>
2012-11-14 09:04:06 -0500

    Update upload css path + adjust migration.

--- db/migrate/20121114135432_update_draft_to_boolean_on_posts.rb
+++ db/migrate/20121114135432_update_draft_to_boolean_on_posts.rb
@@ -1,8 +1,11 @@
 class UpdateDraftToBooleanOnPosts < ActiveRecord::Migration
   def up
-    change_column :wafflemix_posts, :draft, :boolean, :default => false, :null => false
+    remove_column :wafflemix_posts, :draft
+    add_column :wafflemix_posts, :draft, :boolean, :default => false, :null => false
   end
 
   def down
+    remove_column :wafflemix_posts, :draft
+    add_column :wafflemix_posts, :draft, :boolean
   end
 end
