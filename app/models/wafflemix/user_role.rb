778c9d9da349a58b0236835d6689c44e4206c332
James Rissler <risslerjames@gmail.com>
2012-11-20 08:24:41 -0500

    update joins table.

--- app/models/wafflemix/user_role.rb
+++ app/models/wafflemix/user_role.rb
@@ -1,5 +1,8 @@
 module Wafflemix
   class UserRole < ActiveRecord::Base
     attr_accessible :role_id, :user_id
+
+    belongs_to :wafflemix_role, :class_name => 'Wafflemix::Role'
+    belongs_to :wafflemix_user, :class_name => 'Wafflemix::User'
   end
 end
