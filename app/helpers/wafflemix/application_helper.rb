03e093de4e91f973ec42c82569169c27261ec2d6
James Rissler <risslerjames@gmail.com>
2012-11-27 06:33:48 -0500

    Add login/logout to top nav + render home page content.

--- app/helpers/wafflemix/application_helper.rb
+++ app/helpers/wafflemix/application_helper.rb
@@ -45,8 +45,12 @@ module Wafflemix
       t += content_tag(:li, link_to('Files and Images', admin_assets_path))
       t += content_tag(:li, link_to('Categories', admin_categories_path))
       t += content_tag(:li, link_to('Posts', admin_posts_path))
-      t += content_tag(:li, '', :class => 'divider-vertical')
       t += content_tag(:li, link_to('Slides', admin_slides_path))
+      t += content_tag(:li, '', :class => 'divider-vertical')
+      if user_signed_in?
+        t += content_tag(:li, link_to('Logout', destroy_user_session_path, :method => :delete))
+      else
+        t += content_tag(:li, link_to('Login', new_user_session_path))
       t += content_tag(:li, link_to('Customize', '#'))
     end
 
