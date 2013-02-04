2cec8d1eab52887e542b646c0e1d0b8a04a245ef
James Rissler <risslerjames@gmail.com>
2013-02-04 18:38:33 -0500

    Find page for meta info.

--- app/controllers/wafflemix/portfolios_controller.rb
+++ app/controllers/wafflemix/portfolios_controller.rb
@@ -5,6 +5,7 @@ module Wafflemix
 
     def index
       @portfolios = Portfolio.all
+      @page = Page.find_by_link_url('/portfolios')
   
       respond_to do |format|
         format.html
@@ -14,6 +15,7 @@ module Wafflemix
 
     def show
       @portfolio = Portfolio.find(params[:id])
+      @page = Page.find_by_link_url('/portfolios')
   
       respond_to do |format|
         format.html
