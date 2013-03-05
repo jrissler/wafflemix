16df8648a8c5a4f2311dc599f776678951904887
James Rissler <risslerjames@gmail.com>
2013-03-05 06:53:03 -0500

    Home page should go to root url.

--- app/helpers/wafflemix/pages_helper.rb
+++ app/helpers/wafflemix/pages_helper.rb
@@ -2,7 +2,9 @@ module Wafflemix
   module PagesHelper
 
     def link_with_parents(page)
-      if Page.ancestors_of(page).any?
+      if page.link_url == 'home'
+        '/'
+      elsif Page.ancestors_of(page).any?
         '/' + [Page.ancestors_of(page).map{|p| p.title.parameterize}, page.link_url].join('/').gsub(/\/\//, '/')
       else
         '/' + page.link_url
