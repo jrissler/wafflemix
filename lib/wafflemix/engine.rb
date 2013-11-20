f9423a8ccfad0413a279a76dafa06960d66d69d5
jrissler <risslerjames@gmail.com>
2013-11-20 09:27:06 -0500

    precompile engine assets.

--- lib/wafflemix/engine.rb
+++ lib/wafflemix/engine.rb
@@ -1,5 +1,9 @@
 module Wafflemix
   class Engine < ::Rails::Engine
     isolate_namespace Wafflemix
+
+    initializer 'ActiveAdmin precompile hook' do |app|
+    app.config.assets.precompile += ['application.js', 'application.css']
+    end
   end
 end
