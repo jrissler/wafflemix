071521ccf74c90128362bbbb5308b10afd2e1013
jrissler <risslerjames@gmail.com>
2013-11-20 08:39:08 -0500

    Update validation requirements.

--- app/models/wafflemix/contact_form.rb
+++ app/models/wafflemix/contact_form.rb
@@ -2,10 +2,10 @@ module Wafflemix
   class ContactForm < ActiveRecord::Base
     attr_accessible :email, :message, :name, :subject
 
-    validates_presence_of :email, :message, :name, :subject
+    validates_presence_of :email, :name
 
     include Spamster::Model
-    spamster_attrs comment_author: :name, comment_author_email: :email, comment_content: :message
+    spamster_attrs comment_author: :name, comment_author_email: :email
 
     after_create :check_for_spam
 
