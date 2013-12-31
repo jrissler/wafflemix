b65970c63556ff33adeb49231738e9c49ed49bc5
jrissler <risslerjames@gmail.com>
2013-12-31 07:35:18 -0500

    Our required assets for precompile.

--- lib/wafflemix/engine.rb
+++ lib/wafflemix/engine.rb
@@ -3,7 +3,7 @@ module Wafflemix
     isolate_namespace Wafflemix
 
     initializer 'Precompile hook' do |app|
-      app.config.assets.precompile += ['admin/admin_js.js', 'admin/admin.css']
+      app.config.assets.precompile += %w( wafflemix/admin/admin_js.js wafflemix/admin/admin.css jquery-1.7.js mercury.js wafflemix/mercury.js mercury/toolbar.js )
     end
   end
 end
